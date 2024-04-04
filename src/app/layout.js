d4fdc3b0b839c04f3e34f5b1954100e4e3d4fea6
Dani1125 <117635133+Dani1125@users.noreply.github.com>
2024-04-04 19:24:13 +0200

    2024.04.04

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,21 +9,21 @@ import { Toaster } from 'react-hot-toast';
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700']})
 
 export const metadata = {
-  title: 'VirtualTraniner'
+  title: 'VirtualTrainer'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="hu" className="scroll-smooth">
       <body className={roboto.className}>
-        <main className="  mx-auto p-4">
+        <main className="mx-auto">
          <AppProvider>
           <Toaster>
 
           </Toaster>
         <Header/>
         {children}
-        <footer className="border-t p-8 text-center  text-gray-600 mt-16"> 
+        <footer className="border-t p-8 text-center  text-gray-600 "> 
       &copy; 2024 Minden jog fenntartva!
     </footer>
     </AppProvider>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -13,13 +13,13 @@ export default function Header(){
     username = username.split(' ')[0];
   }
     return(
-        <header className="flex mt-2 item-center justify-between p-5 max-w-6xl m-auto">
+        <header className="flex item-center justify-between p-5 max-w-6xl m-auto">
       
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
         <Link className="mr-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={'/'}><img className="w-24 h-34" src="/logo.png"></img></Link>
-         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={'/gyakorlatok'}>Gyakorlatok</Link>
-         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={'/kalkulatorok'}>Kalkulátorok</Link>
-         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={''}>Kapcsolat</Link>
+         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={'/gyakorlatok'}><img className="w-5 h-5 float-left mt-0.5" src="/sulyzo1.png"></img>Gyakorlatok<img className="w-5 h-5 float-right mt-0.5" src="/sulyzo2.png"></img></Link>
+         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={'/kalkulatorok'}><img className="w-5 h-5 float-left mt-0.5" src="/sulyzo1.png"></img>Kalkulátorok<img className="w-5 h-5 float-right mt-0.5" src="/sulyzo2.png"></img></Link>
+         <Link className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={''}><img className="w-5 h-5 float-left mt-0.5" src="/sulyzo1.png"></img>Kapcsolat<img className="w-5 h-5 float-right mt-0.5" src="/sulyzo2.png"></img></Link>
          
       </nav>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -27,7 +27,7 @@ export default function Header(){
       <>
       
       <Link href={'/profil'} className="whitespace-nowrap hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">{username}</Link>
-            <button onClick={() => signOut()} callbackURL={'/'} className="bg-primary text-white px-6 rounded-lg  text-white px-8 py-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-red-700">Kijelentkezés</button>
+            <button onClick={() => signOut()} callbackURL={'/'} className="bg-primary text-white px-6 rounded-lg  text-white px-8 py-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:bg-white hover:text-red-700 hover:border-solid hover:border hover:border-red-700">Kijelentkezés</button>
    
       </>
    
@@ -36,7 +36,7 @@ export default function Header(){
       {status === 'unauthenticated' && (
         <>
        <Link href={'/register'} className="hover:text-red-700 focus:text-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">Regisztráció</Link>
-      <Link href={'/login'} className="bg-primary text-white px-6 rounded-lg  text-white px-8 py-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:bg-white hover:text-black hover:border-solid hover:border hover:border-red-700">Bejelentkezés</Link>
+      <Link href={'/login'} className="bg-primary text-white px-6 rounded-lg  text-white px-8 py-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:bg-white hover:text-red-700 hover:border-solid hover:border hover:border-red-700">Bejelentkezés</Link>
         </>
         )}
       
